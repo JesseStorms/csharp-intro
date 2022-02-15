79b33bfd2645efcb245ae51179c6e162ff305be3
JesseStorms <jesse.storms@student.ucll.be>
2022-02-15 10:55:37 +0100

    Made some stuff

---
+++ exercises/properties/PersonExercise/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonExercise
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            return;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else { this.age = value; }
+            }
+        }
+    }
+}
---
+++ exercises/string-interpolation/FormattingExercise/Formatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingExercise
+{
+    internal class Formatter
+    {
+        public static String Greet(String name)
+        {
+            return $"Hello, {name}";
+        }
+
+        public static String FormatDate(int d, int m, int y)
+        {
+            string day = d.ToString().PadLeft(2, '0');
+            string month = m.ToString().PadLeft(2, '0');
+            return $"{day}/{month}/{y}";
+        }
+
+    }
+}
